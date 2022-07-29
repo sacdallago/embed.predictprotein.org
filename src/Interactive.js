4bb45de16e24c820de630de9babee197820ef8e6
Christian Dallago <code@dallago.us>
2022-07-29 14:25:02 +0200

    interactive works with uniprot accession

--- src/Interactive.js
+++ src/Interactive.js
@@ -4,148 +4,111 @@ import {Container, Stack, Nav, Navbar, Row, Col} from "react-bootstrap";
 
 import FeatureViewer from "./components/FeatureViewer";
 import StructurePrediction from "./components/StructurePrediction";
+import SequenceInput from "./components/SequenceInput";
 
 import "./App.css";
 import {annotationsPlaceholder, structurePlaceholder, resultStatus} from "./stores/JobResults";
 import FeatureViewerLegend from "./components/FeatureViewerLegend";
 import StructureStatus from "./components/StructureStatus";
 import AnnotationsStatus from "./components/AnnotationsStatus";
-
-const ULR = "https://api.bioembeddings.com/api/annotations";
+import storeComponentWrapper from "./stores/jobDispatcher";
+import {proteinStatus} from "./stores/JobParameters";
+import FeatureGrabber from "./components/FeatureGrabber";
 
 class Interactive extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    let sequence = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
+        let sequence = window.location.href.substring(window.location.href.lastIndexOf('/') + 1);
 
-    this.state = {
-      sequence: sequence,
-      features: annotationsPlaceholder,
-      structure: structurePlaceholder,
-    };
-
-    if(sequence.length <= 2000){
-      this.getFeatures(sequence, "prottrans_t5_xl_u50");
-      this.getStructure(sequence);
-    }
-  }
-
-  getFeatures = (sequence, embedder) => {
-    fetch(ULR, {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors, cors, *same-origin
-      // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "same-origin", // include, *same-origin, omit
-      headers: {
-        "Content-Type": "application/json",
-      },
-      redirect: "follow", // manual, *follow, error
-      referrer: "no-referrer", // no-referrer, *client
-      body: JSON.stringify({
-        "sequence": sequence,
-        "format": "full",
-        "model": embedder
-      }), // body data type must match "Content-Type" header
-    })
-        .then(response => response.json())
-        .then(json => {
-          this.setState({
-            features: {
-              ...json,
-              status: resultStatus.DONE
+        this.state = {
+            sequence: sequence,
+            features: annotationsPlaceholder,
+            structure: structurePlaceholder,
+            protein: {
+                uniprotData: {
+                    accession: "P12345"
+                }
             }
-          })
-        })
-        .catch(e => {
-          console.error(e);
-        })
-    ;
-  };
+        };
+    }
 
-  getStructure = (sequence) => {
-    fetch('https://api.bioembeddings.com/api/structure', {
-      method: "POST",
-      mode: "cors",
-      // cache: "no-cache",
-      credentials: "same-origin",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      redirect: "follow",
-      referrer: "no-referrer",
-      body: JSON.stringify({
-        "sequence": sequence,
-        "predictor": "colabfold",
-      }),
-    })
-        .then(response => {
-          return response.json()
-        })
-        .then(json => {
-          // Result is computed
-          if(json.status === "OK") {
-            this.setState({
-              structure: {
-                ...json.structure,
-                status: resultStatus.DONE
-              }
-            })
-          } else {
-            // The request has been created or is being computed!
-            setTimeout(() => this.getStructure(sequence), 5000);
-          }
-        })
-        .catch(e => {
-          console.error(e);
-        })
-    ;
-  };
+    componentWillReceiveProps(nextProps) {
+        let jobParameters = nextProps.jobParameters;
+        let jobResults = nextProps.jobResults;
 
-  render() {
-    return (
-        <div className="App">
-          <Navbar bg="dark" variant="dark">
-            <Container>
-              <Navbar.Brand href="#">Embed Predict Protein (EMPP)</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                  <Nav.Link href="https://github.com/sacdallago/bio_embeddings" target="_blank" rel="noopener noreferrer">
-                    GitHub
-                  </Nav.Link>
-                  <Nav.Link href="https://rostlab.org" target="_blank" rel="noopener noreferrer">
-                    Rostlab Group
-                  </Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
+        if (jobParameters.proteinStatus !== proteinStatus.NULL) {
+            this.setState(
+                {
+                    proteinStatus: jobParameters.proteinStatus,
+                    sequence: jobParameters.protein?.sequence,
+                    protein: jobParameters.protein,
 
-          <Container>
-            <Stack gap={3}>
-              <Row/>
-              <Row>
-                <FeatureViewerLegend />
-              </Row>
-              <Row>
-                <Col>
-                  <AnnotationsStatus sequence={this.state.sequence} features={this.state.features} />
-                  <FeatureViewer data={this.state.features} />
-                </Col>
-              </Row>
-              <Row style={{width: "100%"}}>
-                <Col>
-                  <StructureStatus structure={this.state.structure} sequence={this.state.sequence} />
-                  <StructurePrediction data={this.state.structure.pdb} annotations={this.state.features}/>
-                </Col>
-              </Row>
-              <Row/>
-            </Stack>
-          </Container>
-        </div>
-    );
-  }
+                    // jobParams
+                    embedder: jobParameters.embedder,
+                    predictor: jobParameters.predictor,
+
+                    // Status result
+                    loadingAnnotations: jobResults["prottrans_t5_xl_u50"].status !== resultStatus.DONE,
+                    loadingStructure: jobResults["colabfold"].status !== resultStatus.DONE,
+
+                    // Results
+                    features: jobResults["prottrans_t5_xl_u50"],
+                    structure: jobResults["colabfold"],
+                }
+            );
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Navbar bg="dark" variant="dark">
+                    <Container>
+                        <Navbar.Brand href={"/#/" + (this.state.protein?.uniprotData ? this.state.protein.uniprotData.accession : this.state.sequence )} target={"_blank"}>Embed Predict Protein (EMPP)</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link href="https://github.com/sacdallago/bio_embeddings" target="_blank" rel="noopener noreferrer">
+                                    GitHub
+                                </Nav.Link>
+                                <Nav.Link href="https://rostlab.org" target="_blank" rel="noopener noreferrer">
+                                    Rostlab Group
+                                </Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+
+                <div style={{display: "none"}}>
+                    <SequenceInput/>
+                    <FeatureGrabber />
+                </div>
+
+                <Container>
+                    <Stack gap={3}>
+                        <Row/>
+                        <Row>
+                            <FeatureViewerLegend />
+                        </Row>
+                        <Row>
+                            <Col>
+                                <AnnotationsStatus sequence={this.state.sequence} features={this.state.features} />
+                                <FeatureViewer data={this.state.features} />
+                            </Col>
+                        </Row>
+                        <Row style={{width: "100%"}}>
+                            <Col>
+                                <StructureStatus structure={this.state.structure} sequence={this.state.sequence} />
+                                <StructurePrediction data={this.state.structure.pdb} link={this.state.structure.link} annotations={this.state.features}/>
+                            </Col>
+                        </Row>
+                        <Row/>
+                    </Stack>
+                </Container>
+            </div>
+        );
+    }
 }
 
-export default Interactive;
+export default storeComponentWrapper(Interactive);
