977ad480a9abd0998c1205bcb18b231b119da5b1
Tobias O <tobias.olenyi@tum.de>
2022-09-15 04:27:19 +0000

    Add minimal input validation capabilities

--- src/components/SequenceInput.new.jsx
+++ src/components/SequenceInput.new.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 
-const InputType = {
-    fasta: Symbol("fasta"),
-    residue: Symbol("residue"),
-    uniprot_id: Symbol("uniprot"),
-    uniprot_protein_name: Symbol("protein_name"),
-};
+import { Form, Col, Row } from "react-bootstrap";
+
+import { eval_input_type, InputType } from "../utils/sequence";
 
 const example_input = {
     [InputType.fasta]: `>My sequence
@@ -24,6 +21,7 @@ QRPSSRASSRASSRPRPDDLEI`,
 
 export const SequenceInput = (props) => {
     const [sequence, setSequence] = React.useState("");
+    const ref_input = React.createRef();
 
     if (props.sequence !== "") {
         setSequence(props.sequence);
@@ -34,43 +32,76 @@ export const SequenceInput = (props) => {
         setSequence(input);
     };
 
+    const validate_input = (input) => {
+        return eval_input_type(input) !== InputType.invalid;
+    };
+
     return (
-        <form>
-            <textarea id="w3review" name="w3review" rows="5" value={sequence} />
-            <div>
-                Input a sequence in either{" "}
-                <span
-                    className="clickable-example"
-                    onClick={() => setExampleState(InputType.fasta)}
-                >
-                    FASTA format
-                </span>
-                , a {""}
-                <span
-                    className="clickable-example"
-                    onClick={() => setExampleState(InputType.uniprot_id)}
-                >
-                    UniProt Accession
-                </span>{" "}
-                number or {""}
-                <span
-                    className="clickable-example"
-                    onClick={() =>
-                        setExampleState(InputType.uniprot_protein_name)
-                    }
-                >
-                    UniProt Protein Name
-                </span>
-                , or {""}
-                <span
-                    className="clickable-example"
-                    onClick={() => setExampleState(InputType.residue)}
-                >
-                    AA sequence
-                </span>
-                .
-            </div>
-        </form>
+        <Form className="d-flex justify-content-center mt-3">
+            <Form.Group controlId="sequenceInput">
+                <Row>
+                    <Col md={10}>
+                        <Form.Control
+                            as="textarea"
+                            rows={5}
+                            cols={100}
+                            ref={ref_input}
+                            value={sequence}
+                            placeholder="SEQWENCE..."
+                            onChange={(event) => {
+                                setSequence(ref_input.current.value);
+                                var [seq_type, seq_alphabet] = eval_input_type(
+                                    ref_input.current.value
+                                );
+                            }}
+                        />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={10}>
+                        <Form.Text muted={true}>
+                            Input a sequence in either{" "}
+                            <span
+                                className="clickable-example"
+                                onClick={() => setExampleState(InputType.fasta)}
+                            >
+                                FASTA format
+                            </span>
+                            , a {""}
+                            <span
+                                className="clickable-example"
+                                onClick={() =>
+                                    setExampleState(InputType.uniprot_id)
+                                }
+                            >
+                                UniProt Accession
+                            </span>{" "}
+                            number or {""}
+                            <span
+                                className="clickable-example"
+                                onClick={() =>
+                                    setExampleState(
+                                        InputType.uniprot_protein_name
+                                    )
+                                }
+                            >
+                                UniProt Protein Name
+                            </span>
+                            , or {""}
+                            <span
+                                className="clickable-example"
+                                onClick={() =>
+                                    setExampleState(InputType.residue)
+                                }
+                            >
+                                AA sequence
+                            </span>
+                            .
+                        </Form.Text>
+                    </Col>
+                </Row>
+            </Form.Group>
+        </Form>
     );
 };
 
---
+++ src/utils/sequence.js
@@ -0,0 +1,61 @@
+export const InputType = {
+    fasta: Symbol("fasta"),
+    residue: Symbol("residue"),
+    uniprot_id: Symbol("uniprot"),
+    uniprot_protein_name: Symbol("protein_name"),
+    invalid: Symbol("invalid"),
+};
+
+export const Alphabet = {
+    iupac: Symbol("iupac"),
+    iupac_extended: Symbol("extended"),
+    undefined: Symbol("undefined"),
+};
+
+const IUPAC = "ACDEFGHIKLMNPQRSTVWYX";
+const IUPAC_extended = IUPAC + "BZJUO";
+
+const re_accessionNumber = new RegExp(
+    "[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}$"
+);
+const re_uniprotName = new RegExp("[A-Z0-9]{3,20}_[A-Z0-9]{3,20}");
+const re_invalid_aminoAcid = new RegExp("[^" + IUPAC + "]");
+const re_invalid_aminoAcid_extended = new RegExp("[^" + IUPAC_extended + "]");
+const re_fasta_header = new RegExp("^>.*$");
+
+function get_sequence_details(input) {
+    let alphabet = Alphabet.undefined;
+    let type = InputType.invalid;
+
+    if (!re_invalid_aminoAcid.test(input)) {
+        alphabet = Alphabet.iupac;
+        type = InputType.residue;
+    } else if (!re_invalid_aminoAcid_extended.test(input)) {
+        alphabet = Alphabet.iupac_extended;
+        type = InputType.residue;
+    }
+    return [type, alphabet];
+}
+
+export function eval_input_type(input) {
+    let test_str = input.toUpperCase();
+    let type = InputType.invalid;
+    let alphabet = Alphabet.undefined;
+
+    if (re_uniprotName.test(test_str)) {
+        type = InputType.uniprot_protein_name;
+    } else if (re_accessionNumber.test(test_str)) {
+        type = InputType.uniprot_id;
+    } else {
+        [type, alphabet] = get_sequence_details(test_str);
+        if (type === InputType.invalid) {
+            let lines = test_str.split(/\r?\n/);
+            if (lines.length > 1 && re_fasta_header.test(lines[0])) {
+                [, alphabet] = get_sequence_details(lines.slice(1).join(""));
+                if (alphabet !== Alphabet.undefined) type = InputType.fasta;
+            }
+        }
+    }
+
+    return [type, alphabet];
+}
