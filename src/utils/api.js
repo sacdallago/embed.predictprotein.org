2197e26b1381b9ab33e6029c34f92ba5d08ca148
Tobias O <tobias.olenyi@tum.de>
2022-10-01 01:49:40 +0000

    Add proper status query

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,1 +1,10 @@
-export function get_worker_status(worker_name) {}
+const Endpoint = "https://api.bioembeddings.com/api/";
+
+export async function get_worker_status(worker_name) {
+    const status_endpoint = Endpoint + "status/";
+    const response = await fetch(status_endpoint + worker_name);
+    if (!response.ok) {
+        throw new Error("Worker status not okay.");
+    }
+    return response.json();
+}
